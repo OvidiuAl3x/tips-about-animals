19762153ab32352482afebafbc175c116d7d9e9d
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2023-06-23 15:27:15 +0300

    add animation/food page/animation active scroll

--- js/service/dog-data.js
+++ js/service/dog-data.js
@@ -1,6 +1,11 @@
 import data from "./db.json" assert { type: "json" };
 
 const { history, food, treats, bowls, bed, leashes, collars, toys } = data.dog;
-const get = document.querySelector(".dog-history");
+const getHistory = document.querySelector(".dog-history");
+const getFood = document.querySelectorAll(".dog-food");
 // get.innerHTML = history + food + treats + `<h1>${bed}</h1>`;
-get.innerHTML = history;
+getHistory.innerHTML += history;
+
+getFood.forEach(function (element) {
+  element.innerHTML += food;
+});
