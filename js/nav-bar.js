0fd069470304fab49dbc1cf795e93929a04361d6
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2023-06-23 16:17:29 +0300

    nav active on scroll

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,10 +1,13 @@
 const nav = document.getElementById("nav-js");
-const closeNav = document.querySelector(".open-nav");
+const openNavButton = document.querySelector(".open-nav");
+const closeNav = document.querySelector(".close-nav");
 
 // open nav
 function openNav() {
   nav.classList.toggle("open-aside");
+  openNavButton.classList.toggle("display-none");
 }
+openNavButton.addEventListener("click", openNav);
 closeNav.addEventListener("click", openNav);
 
 // open dropdown from link to another page
@@ -16,10 +19,32 @@ selectPage.addEventListener("click", function () {
 });
 
 // automatic close nav when pressed on link
-const selectClose = document.querySelectorAll(".select");
+const menu = document.querySelectorAll(".select");
 
-selectClose.forEach(function (e) {
+menu.forEach(function (e) {
   e.addEventListener("click", function () {
     nav.classList.remove("open-aside");
+    openNavButton.classList.toggle("display-none");
   });
 });
+
+// active nav links on scroll
+// we use const menu from above
+let section = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("select-active");
+        document
+          .querySelector(".nav li a[href*=" + id + "]")
+          .classList.add("select-active");
+      });
+    }
+  });
+};
