294f777782387293d24864fde132a07f36bce2e3
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2023-06-26 16:31:16 +0300

    destructuring

--- js/service/dog-data.js
+++ js/service/dog-data.js
@@ -1,18 +1,31 @@
 import data from "./db.json" assert { type: "json" };
 
-const { history, origins, breeds, working_dogs, living, leashes, collars, toys } = data.dog;
-const getHistory = document.querySelector(".dog-history");
-const getorigins = document.querySelectorAll(".dog-origins");
+const {
+  history,
+  origins,
+  breeds,
+  working_dogs,
+  living,
+  leashes,
+  collars,
+  toys,
+} = data.dog;
+// const getHistory = document.querySelector(".dog-text");
+const getorigins = document.querySelectorAll(".dog-text");
 const getbreeds = document.querySelectorAll(".dog-breeds");
 const getWorking = document.querySelectorAll(".working-dogs-p");
-getHistory.innerHTML += history;
+const getLiving = document.querySelectorAll(".dog-living");
+// getHistory.innerHTML += history;
 
 getorigins.forEach(function (element) {
   element.innerHTML += origins;
 });
-getbreeds.forEach(function (element) {
-  element.innerHTML += breeds;
-});
-getWorking.forEach(function (element) {
-  element.innerHTML += working_dogs;
-});
+// getbreeds.forEach(function (element) {
+//   element.innerHTML += breeds;
+// });
+// getWorking.forEach(function (element) {
+//   element.innerHTML += working_dogs;
+// });
+// getLiving.forEach(function (element) {
+//   element.innerHTML += living;
+// });
